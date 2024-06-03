759c70a098c66be58c2e4896d921b634c8e2494c
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-06-03 20:23:27 +0200

    Fixed tests to match correct serialization

--- test/trpc_client_test.dart
+++ test/trpc_client_test.dart
@@ -7,18 +7,19 @@ void main() {
   group('Query input serialization', () {
     test('JSON parsing', () {
       expect(serializeQueryInput({"testing": true, "userId": "userId"}),
-          "%7B%22testing%22%3Atrue%2C%22userId%22%3A%22userId%22%7D");
+          "%7B%22json%22%3A%7B%22testing%22%3Atrue%2C%22userId%22%3A%22userId%22%7D%7D");
     });
 
     test('String parsing', () {
-      expect(serializeQueryInput("Testing"), "%22Testing%22");
+      expect(
+          serializeQueryInput("Testing"), "%7B%22json%22%3A%22Testing%22%7D");
     });
   });
 
   group('Batch Query input serialization', () {
     test('tRPC Example', () {
-      expect(serializeQueryInputBatch(["1", "1"]),
-          "%7B%220%22%3A%221%22%2C%221%22%3A%221%22%7D");
+      expect(serializeQueryInputBatch(["1", "2"]),
+          "%7B%220%22%3A%7B%22json%22%3A%221%22%7D%2C%221%22%3A%7B%22json%22%3A%222%22%7D%7D");
     });
 
     test('Complex parsing', () {
@@ -27,7 +28,7 @@ void main() {
             {"userId": 69, "page": 1, "offset": "alot"},
             {"userId": -1, "page": 999}
           ]),
-          "%7B%220%22%3A%7B%22userId%22%3A69%2C%22page%22%3A1%2C%22offset%22%3A%22alot%22%7D%2C%221%22%3A%7B%22userId%22%3A-1%2C%22page%22%3A999%7D%7D");
+          "%7B%220%22%3A%7B%22json%22%3A%7B%22userId%22%3A69%2C%22page%22%3A1%2C%22offset%22%3A%22alot%22%7D%7D%2C%221%22%3A%7B%22json%22%3A%7B%22userId%22%3A-1%2C%22page%22%3A999%7D%7D%7D");
     });
   });
 
