ee4063001d2bdb128239782f69cd6304bd0e4aa3
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-10-23 10:02:44 +0100

    added more files to the annotations

--- trpc_generator/lib/trpc_annotations.dart
+++ trpc_generator/lib/trpc_annotations.dart
@@ -3,11 +3,15 @@ class TrpcAnnotations {
   final String packageName;
   final String outputFilePath;
   final List<String> generatedFor;
+  final bool generatedFreezed;
+  final List<String> freezedGeneratedfor;
 
   const TrpcAnnotations(
     this.jsonFilePath, {
     this.packageName = 'trpcGenerator',
     this.outputFilePath = 'lib/generated/',
-    this.generatedFor = const [],
+    this.generatedFor = const ['lib/trpc_routes.dart'],
+    this.generatedFreezed = false,
+    this.freezedGeneratedfor = const ['lib/generated/trpc_routes.dart'],
   });
 }
