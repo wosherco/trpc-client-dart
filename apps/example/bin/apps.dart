c9b6a3bc2326d3d5ed205ffbcd8595ba0344def2
polvallverdu <pvallverdualcaide@gmail.com>
2025-01-22 21:48:28 +0100

    fix: correct pathname for example

--- apps/example/bin/apps.dart
+++ apps/example/bin/apps.dart
@@ -2,7 +2,8 @@ import 'package:trpc_client/trpc_client.dart';
 import 'package:trpc_client_example/trpc.dart';
 
 void main() async {
-  final client = RoutedTrpcClient(TRPCClient(baseUri: "http://localhost:3000"));
+  final client =
+      RoutedTrpcClient(TRPCClient(baseUri: "http://localhost:3000/trpc"));
 
   final test = await client.nested.nestedx2.hello7.query();
   print(test.hello);
