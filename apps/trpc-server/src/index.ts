dd8ebd5689130baf1c330713f51c6ca837219496
polvallverdu <pvallverdualcaide@gmail.com>
2025-01-22 21:48:19 +0100

    chore: added logger to example server

--- apps/trpc-server/src/index.ts
+++ apps/trpc-server/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import { trpcServer } from "@hono/trpc-server";
 import { appRouter } from "./router";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+app.use("*", logger());
 
 app.use(
   "/trpc/*",
